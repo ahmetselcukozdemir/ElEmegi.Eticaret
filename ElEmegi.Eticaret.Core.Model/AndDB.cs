89aa87e799667fa7ba18fcbf65156d17863aa0a8
Ahmet Selçuk Özdemir <ahmetselcukozdemir01@gmail.com>
2020-04-25 01:31:40 +0300

    commit1

---
+++ ElEmegi.Eticaret.Core.Model/AndDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElEmegi.Eticaret.Core.Model
+{
+   public class AndDB : DbContext
+    {
+        //tablolar
+    }
+}
